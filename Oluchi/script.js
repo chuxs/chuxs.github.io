2d041ebf2a94f56d2af73da4387de52c777ab734
chuxs <42435519+chuxs@users.noreply.github.com>
2023-04-22 17:55:42 +0100

    pop added

--- Oluchi/script.js
+++ Oluchi/script.js
@@ -27,37 +27,49 @@ function topFunction() {
 // let menu = document.get("#menu-icon");
 let menu = document.getElementById("menu-icon")
 let navbar = document.querySelector(".navbar");
+let cartbtn = document.getElementById("cartbtn");
+let popup = document.getElementById("popup");
+let closebtn = document.getElementsByClassName("close-btn")[0];
 
-// menu.addEventListener("click", function () {
-//     navbar.classList.toggle("active")
-//     // menu.classList = "bx bx-right-arrow-alt"; bx bx-menu
-//     if (document.getElementById(menu-icon).className == "bx bx-menu") {
-//         document.getElementById(menu-icon).className = "bx bx-right-arrow-alt";
-//     }
-//     else {
-//         document.getElementById(menu-icon).className = "bx bx-menu"
-//     }
-// });
-
-function changeIcon(memuarrow) {
-    navbar.classList.toggle("active")
-    if (document.getElementById(memuarrow).className == "bx bx-menu") {
-                document.getElementById(memuarrow).className = "bx bx-right-arrow-alt";
-            }
-            else {
-                document.getElementById(memuarrow).className = "bx bx-menu";
-            }
+cartbtn.onclick = function () {
+    popup.classList.add("show");
+    navbar.classList.remove("active");
+    document.getElementById(menu).className = "bx bx-menu";
 }
+
+closebtn.onclick = function () {
+    popup.classList.remove("show");
+}
+
+menu.addEventListener("click", function () {
+    navbar.classList.toggle("active")
+    // menu.classList = "bx bx-right-arrow-alt"; bx bx-menu
+    // if (document.getElementById(menu-icon).className == "bx bx-menu") {
+    //     document.getElementById(menu-icon).className = "bx bx-right-arrow-alt";
+    // }
+    // else {
+    //     document.getElementById(menu-icon).className = "bx bx-menu"
+    // }
+});
+
+// function changeIcon(memuarrow) {
+//     navbar.classList.toggle("active")
+//     // if (document.getElementById(memuarrow).className == "bx bx-menu") {
+//     //     document.getElementById(memuarrow).className = "bx bx-right-arrow-alt";
+//     // }
+//     // else {
+//     //     document.getElementById(memuarrow).className = "bx bx-menu";
+//     // }
+// }
 
 window.onscroll = () => {
     navbar.classList.remove("active");
     // document.getElementById(menu-icon).className = "bx bx-menu";
 };
 
-body.addEventListener("click", clicker);
-
-function clicker() {
-    if (menu.className == "bx bx-right-arrow-alt") {
-        menu.className = "bx bx-menu";
-    }
-}
+// body.addEventListener("click", clicker);
+// function clicker() {
+//     if (menu.className == "bx bx-right-arrow-alt") {
+//         menu.className = "bx bx-menu";
+//     }
+// }
