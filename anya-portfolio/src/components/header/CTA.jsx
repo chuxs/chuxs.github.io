edad109ed4ceee77b69f02b13ec7cd784602ad52
chuxs <42435519+chuxs@users.noreply.github.com>
2023-11-27 05:41:26 +0100

    done with components-started header

---
+++ anya-portfolio/src/components/header/CTA.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const CTA = () => {
+  return (
+    <div className='cta'> 
+        <a href="">Download Resume</a>
+        <a href="#contact">Contact Me</a>
+    </div>
+  )
+}
+
+export default CTA
