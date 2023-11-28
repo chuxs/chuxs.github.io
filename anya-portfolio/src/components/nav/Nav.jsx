f3a2ce4348c30d9dfa7c057dedd7762436c24252
chuxs <42435519+chuxs@users.noreply.github.com>
2023-11-29 00:37:23 +0100

    about changes

--- anya-portfolio/src/components/nav/Nav.jsx
+++ anya-portfolio/src/components/nav/Nav.jsx
@@ -4,14 +4,20 @@ import { RiHomeGearLine } from "react-icons/ri";
 import { FaUserCircle } from "react-icons/fa";
 import { FaProjectDiagram } from "react-icons/fa";
 import { BiSolidMessageDetail } from "react-icons/bi";
+import { useState } from 'react';
 
 const Nav = () => {
+  const [activeNav, setActiveNav] = useState('#')
   return (
     <nav>
-      <a href="#home"><RiHomeGearLine /></a>
-      <a href="#about"><FaUserCircle /></a>
-      <a href="#portfolio"><FaProjectDiagram /></a>
-      <a href="#contact"><BiSolidMessageDetail /></a>
+      <a href="/#" onClick={() => setActiveNav('#')} className={activeNav === '#' ? 'active' : ''}>
+        <RiHomeGearLine /></a>
+      <a href="#about" onClick={() => setActiveNav('#about')} className={activeNav === '#about' ? 'active' : ''}>
+        <FaUserCircle /></a>
+      <a href="#portfolio" onClick={() => setActiveNav('#portfolio')} className={activeNav === '#portfolio' ? 'active' : ''}>
+        <FaProjectDiagram /></a>
+      <a href="#contact" onClick={() => setActiveNav('#contact')} className={activeNav === '#contact' ? 'active' : ''}>
+        <BiSolidMessageDetail /></a>
     </nav>
   )
 }
