d098205b9f1b329676addadc99d1bf2f4bf60cf7
chuxs <42435519+chuxs@users.noreply.github.com>
2023-11-28 09:22:21 +0100

    Mostly Nav and Header Changes

--- anya-portfolio/src/components/nav/Nav.jsx
+++ anya-portfolio/src/components/nav/Nav.jsx
@@ -1,9 +1,18 @@
 import React from 'react'
 import './nav.css'
+import { RiHomeGearLine } from "react-icons/ri";
+import { FaUserCircle } from "react-icons/fa";
+import { FaProjectDiagram } from "react-icons/fa";
+import { BiSolidMessageDetail } from "react-icons/bi";
 
 const Nav = () => {
   return (
-    <div>Nav</div>
+    <nav>
+      <a href="#home"><RiHomeGearLine /></a>
+      <a href="#about"><FaUserCircle /></a>
+      <a href="#portfolio"><FaProjectDiagram /></a>
+      <a href="#contact"><BiSolidMessageDetail /></a>
+    </nav>
   )
 }
 
