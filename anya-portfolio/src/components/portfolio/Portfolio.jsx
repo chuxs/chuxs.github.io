f7bef6c829ed02d91efaa361da350b930aeadfa1
chuxs <42435519+chuxs@users.noreply.github.com>
2023-12-02 17:40:28 +0100

    dont with porfolio

--- anya-portfolio/src/components/portfolio/Portfolio.jsx
+++ anya-portfolio/src/components/portfolio/Portfolio.jsx
@@ -1,11 +1,51 @@
 import React from 'react'
 import './portfolio.css'
-import IMGA from '../../assests/ecommercestore.png'
-import IMGB from '../../assests/Firbasechatapp.png'
-import IMGC from '../../assests/Google map api Distance and time calculator.png'
-import IMGD from '../../assests/lulusnativekitchen.png'
+import IMGA from '../../assests/Firbasechatapp.png'
+import IMGB from '../../assests/ecommercestore.png'
+import IMGC from '../../assests/lulusnativekitchen.png'
+import IMGD from '../../assests/Google map api Distance and time calculator.png'
 import IMGE from '../../assests/Proactive validation form.png'
 import IMGF from '../../assests/Simplecalculator.png'
+
+const data = [
+  {
+    id:1,
+    image: IMGA,
+    title: 'Firebase Chat App',
+    demo: 'https://chuxs.github.io/Chatting Programm JavaScript/index.html'
+  },
+  {
+    id:2,
+    image: IMGB,
+    title: 'Simple Shopping Site',
+    demo: 'https://chuxs.github.io/ChuxLiveStore/index.html'
+  },
+  {
+    id:3,
+    image: IMGC,
+    title: 'Food Ordering Service',
+    demo: 'http://lulusnativekitchen.com/'
+  },
+  {
+    id:4,
+    image: IMGD,
+    title: 'Google Map API Distance and Time Calculator',
+    demo: 'https://chuxs.github.io/JavaScript Google Map Api Project/index.html'
+  },
+  {
+    id:5,
+    image: IMGE,
+    title: 'Proactive Validation Form',
+    demo: 'https://chuxs.github.io/JavaScript Form Validation/index.html'
+  },
+  {
+    id:6,
+    image: IMGF,
+    title: 'Simple Calculator',
+    demo: 'https://chuxs.github.io/Calculator/index.html'
+  }
+
+]
 
 const Portfolio = () => {
   return (
@@ -14,74 +54,22 @@ const Portfolio = () => {
       <h2>Projects</h2>
 
       <div className="container portfolio__container">
-
-        <article className='portfolio__item'>
-          <div className="portfolio__item-image">
-            <img src={IMGA} alt="store" />
-          </div>
-          <h4>This is a Poject Title</h4>
-          <div className="portfolio_item-cta"> 
-          <a href="https://chuxs.github.io/ChuxLiveStore/index.html" className='btn btn-primary' rel="noreferrer" target='_blank'> 
-          Demo </a> 
-          </div>
-        </article>
-
-        <article className='portfolio__item'>
-          <div className="portfolio__item-image">
-            <img src={IMGB} alt="store" />
-          </div>
-          <h4>This is a Poject Title</h4>
-          <div className="portfolio_item-cta"> 
-          <a href="https://chuxs.github.io/ChuxLiveStore/index.html" className='btn btn-primary' rel="noreferrer" target='_blank'> 
-          Demo </a> 
-          </div>
-        </article>
-
-        <article className='portfolio__item'>
-          <div className="portfolio__item-image">
-            <img src={IMGC} alt="store" />
-          </div>
-          <h4>This is a Poject Title</h4>
-          <div className="portfolio_item-cta"> 
-          <a href="https://chuxs.github.io/ChuxLiveStore/index.html" className='btn btn-primary' rel="noreferrer" target='_blank'> 
-          Demo </a> 
-          </div>
-        </article>
-
-        <article className='portfolio__item'>
-          <div className="portfolio__item-image">
-            <img src={IMGD} alt="store" />
-          </div>
-          <h4>This is a Poject Title</h4>
-          <div className="portfolio_item-cta"> 
-          <a href="https://chuxs.github.io/ChuxLiveStore/index.html" className='btn btn-primary' rel="noreferrer" target='_blank'> 
-          Demo </a> 
-          </div>
-        </article>
-
-        <article className='portfolio__item'>
-          <div className="portfolio__item-image">
-            <img src={IMGE} alt="store" />
-          </div>
-          <h4>This is a Poject Title</h4>
-          <div className="portfolio_item-cta"> 
-          <a href="https://chuxs.github.io/ChuxLiveStore/index.html" className='btn btn-primary' rel="noreferrer" target='_blank'> 
-          Demo </a> 
-          </div>
-        </article>
-
-        <article className='portfolio__item'>
-          <div className="portfolio__item-image">
-            <img src={IMGF} alt="store" />
-          </div>
-          <h4>This is a Poject Title</h4>
-          
-          <div className="portfolio_item-cta"> 
-          <a href="https://chuxs.github.io/ChuxLiveStore/index.html" className='btn btn-primary' rel="noreferrer" target='_blank'> 
-          Demo </a> 
-          </div>
-        </article>
-
+        {
+          data.map(({id, image, title, demo}) => {
+            return(
+              <article key={id} className='portfolio__item'>
+                <div className="portfolio__item-image">
+                  <img src={image} alt={title} />
+                </div>
+                <h4>{title}</h4>
+                <div className="portfolio_item-cta"> 
+                 <a href={demo} className='btn btn-primary' rel="noreferrer" target='_blank'> 
+                 Demo </a> 
+                </div>
+              </article>
+            )
+          })
+        }
       </div>
     </section>
   )
